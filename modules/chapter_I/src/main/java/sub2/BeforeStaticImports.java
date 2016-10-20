a8b5ce9485cbb3d53f56578d8a905f7ce81b2a7a
Iuliana Danea <iuliana.danea@ness.com>
2016-10-20 16:23:53 +0300

    Changed package name(sub-chapter 2:Define Classes )

---
+++ modules/chapter_I/src/main/java/sub2/BeforeStaticImports.java
@@ -0,0 +1,20 @@
+package sub2;
+
+/**
+ * Chapter I: Declaration and Data Access
+ *
+ * SubChapter 2:Define Classes
+ *
+ * SubSubChapter5: Static Import Statement
+ *
+ * Launching applications with <i>java</i>
+ */
+public class StaticImports
+{
+
+    public static void main(String... x)
+    {
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.toHexString(42));
+    }
+}
