1e9bf9e3b307ec8fa4a1367018dc6bc08fc2c96e
Iuliana Danea <iuliana.danea@ness.com>
2016-10-20 13:55:21 +0300

    Initial commit with project setup

---
+++ modules/chapter_I/src/main/java/define-classes/MyClass.java
@@ -0,0 +1,14 @@
+
+/**
+ * Chapter I, Declaration and Data Access. Section2, Define Classes.
+ *
+ * Launching Applications with Java
+ */
+public class MyClass {
+
+    public static void main(String args[]){
+
+        System.out.println(args[0]+" "+ args[1]);
+
+    }
+}
