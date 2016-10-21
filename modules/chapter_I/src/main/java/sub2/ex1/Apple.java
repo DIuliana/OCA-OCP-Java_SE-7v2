bf43bcfdae20e61a3964c340becb55251650e3a4
Iuliana Danea <iuliana.danea@ness.com>
2016-10-21 11:40:49 +0300

    Exercise 1.1

---
+++ modules/chapter_I/src/main/java/sub2/ex1/Apple.java
@@ -0,0 +1,17 @@
+package sub2.ex1.food;
+
+import sub2.ex1.food.Fruit;
+
+/**
+ * Chapter I: Declaration and Data Access
+ *
+ * SubChapter 2:Define Classes
+ *
+ * SubSubChapter6: Class Declaration and Modifiers
+ *
+ * Exercise I-I
+ */
+class Apple extends Fruit {
+
+
+}
---
+++ modules/chapter_I/src/main/java/sub2/ex1/food/Fruit.java
@@ -0,0 +1,15 @@
+package sub2.ex1;
+
+/**
+ * Chapter I: Declaration and Data Access
+ *
+ * SubChapter 2:Define Classes
+ *
+ * SubSubChapter6: Class Declaration and Modifiers
+ *
+ * Exercise I-I
+ */
+public abstract class Fruit {
+
+
+}
