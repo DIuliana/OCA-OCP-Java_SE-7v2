dcfda90b2bba5ba3a870b8d5a788a7fdc019bf89
Iuliana Danea <iuliana.danea@ness.com>
2016-10-24 10:50:25 +0300

    Used AOP to declare Minstral as an aspect.

--- modules/spring/src/main/java/org/springinaction/knights/BraveKnight.java
+++ modules/spring/src/main/java/org/springinaction/knights/BraveKnight.java
@@ -7,18 +7,15 @@ public class BraveKnight implements Knight
 {
 
     Quest quest;
-    Minstrel minstrel;
 
-    public BraveKnight(Quest quest, Minstrel minstrel)
+
+    public BraveKnight(Quest quest)
     {
         this.quest = quest;
-        this.minstrel = minstrel;
     }
 
     public void embarkOnQuest()
     {
-        minstrel.singBeforeQuest();
         quest.embark();
-        minstrel.singAfterQuest();
     }
 }
