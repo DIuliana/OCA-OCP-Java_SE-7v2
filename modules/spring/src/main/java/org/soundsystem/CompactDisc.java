eb4d06c57c5340efb319b575cbea4e548f713bf6
Iuliana Danea <iuliana.danea@ness.com>
2016-10-26 14:16:21 +0300

    Example of  Creating discoverable beans.

---
+++ modules/spring/src/main/java/org/soundsystem/CompactDisc.java
@@ -0,0 +1,12 @@
+package org.soundsystem;
+
+/**
+ * Part I: Core Spring 2. Wiring beans
+ *
+ * 2.2.1 Automatically wiring beans
+ *
+ */
+public interface CompactDisc {
+
+    void play();
+}
---
+++ modules/spring/src/main/java/org/soundsystem/SgtPeppers.java
@@ -0,0 +1,24 @@
+package org.soundsystem;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Part I: Core Spring 2. Wiring beans
+ *
+ * 2.2.1 Automatically wiring beans
+ *
+ */
+
+@Component
+public class SgtPeppers implements CompactDisc
+{
+
+    private String title = "Sgt. Pepper's Lonely Hearts Club Band";
+    private String artist = "The Beatles";
+
+    public void play()
+    {
+        System.out.println("Playing " + title + " by " + artist);
+
+    }
+}
