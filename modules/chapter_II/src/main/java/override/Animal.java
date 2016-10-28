62aa3efbbd7dd5ddc57620183dd6817a2d207fe5
Iuliana Danea <iuliana.danea@ness.com>
2016-10-28 12:41:36 +0300

    Override. JVM dynamicaly associates the object refered to the acuatl instance object, so it knows witch method to call

---
+++ modules/chapter_II/src/main/java/override/Animal.java
@@ -0,0 +1,12 @@
+package override;
+
+/**
+ * Created by p3700639 on 10/27/2016.
+ */
+public class Animal {
+
+    public void eat(){
+
+        System.out.println("I am an Animal and i eat generically food");
+    }
+}
---
+++ modules/chapter_II/src/main/java/override/Horse.java
@@ -0,0 +1,17 @@
+package override;
+
+/**
+ * Created by p3700639 on 10/27/2016.
+ */
+public class Horse extends Animal {
+
+    public void eat(){
+
+        System.out.println("I am a Horse and i eat fan!");
+    }
+
+    public void buck(){
+
+
+    }
+}
